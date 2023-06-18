a5fafa005c9b480ed301b12058975d99f1fadea9
mesut-dnr <mesutdonerng@gmail.com>
2023-06-18 19:31:53 +0300

    product aggregate-command-event added

--- product-services/src/main/java/com/mdonerprojects/productservices/controller/ProductController.java
+++ product-services/src/main/java/com/mdonerprojects/productservices/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.mdonerprojects.productservices.controller;
 
+import com.mdonerprojects.productservices.command.CreateProductCommand;
 import com.mdonerprojects.productservices.rest.CreateProductRestModel;
 import lombok.RequiredArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/api/v1")
@@ -11,10 +15,24 @@ import org.springframework.web.bind.annotation.*;
 public class ProductController {
 
     private final Environment env;
+
+    private final CommandGateway commandGateway;
     @PostMapping(value = "/save")
     public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
 
-        return "HTTP POST Handled with:"+createProductRestModel.getTitle();
+        CreateProductCommand createProductCommand = CreateProductCommand.builder()
+                .productId(UUID.randomUUID().toString())
+                .price(createProductRestModel.getPrice())
+                .title(createProductRestModel.getTitle())
+                .quantity(createProductRestModel.getQuantity())
+                .build();
+        String returnValue = null;
+        try {
+            returnValue = commandGateway.sendAndWait(createProductCommand);
+        } catch (Exception e) {
+            returnValue=e.getLocalizedMessage();
+        }
+        return "Return value:"+returnValue;
     }
 
     @GetMapping
