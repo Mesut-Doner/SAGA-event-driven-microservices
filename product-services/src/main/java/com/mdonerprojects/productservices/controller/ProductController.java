7ed45a4dc13439d2936cfb1c5daec4adeb8301ed
mesut-dnr <mesutdonerng@gmail.com>
2023-06-18 18:52:57 +0300

    prodct rest model added

--- product-services/src/main/java/com/mdonerprojects/productservices/controller/ProductController.java
+++ product-services/src/main/java/com/mdonerprojects/productservices/controller/ProductController.java
@@ -1,18 +1,20 @@
 package com.mdonerprojects.productservices.controller;
 
+import com.mdonerprojects.productservices.rest.CreateProductRestModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping(value = "/api/v1")
 @RequiredArgsConstructor
 public class ProductController {
 
     private final Environment env;
-    @PostMapping
-    public String createProduct() {
-        return "HTTP POST Handled";
+    @PostMapping(value = "/save")
+    public String createProduct(@RequestBody CreateProductRestModel createProductRestModel) {
+
+        return "HTTP POST Handled with:"+createProductRestModel.getTitle();
     }
 
     @GetMapping
