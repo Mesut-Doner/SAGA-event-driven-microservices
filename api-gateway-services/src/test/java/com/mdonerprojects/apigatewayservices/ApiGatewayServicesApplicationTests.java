d112ca04bad43138bf6486d51451ae2851b671bd
mesut-dnr <mesutdonerng@gmail.com>
2023-06-18 16:08:29 +0300

    api-gateway-service init

---
+++ api-gateway-services/src/test/java/com/mdonerprojects/apigatewayservices/ApiGatewayServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdonerprojects.apigatewayservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
