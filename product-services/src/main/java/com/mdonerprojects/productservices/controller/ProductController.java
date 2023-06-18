48a3e32ba6d3b41303f21e553d21ead74dcbd60c
mesut-dnr <mesutdonerng@gmail.com>
2023-06-18 16:53:24 +0300

    apigateway test

--- product-services/src/main/java/com/mdonerprojects/productservices/controller/ProductController.java
+++ product-services/src/main/java/com/mdonerprojects/productservices/controller/ProductController.java
@@ -1,11 +1,15 @@
 package com.mdonerprojects.productservices.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final Environment env;
     @PostMapping
     public String createProduct() {
         return "HTTP POST Handled";
@@ -13,7 +17,7 @@ public class ProductController {
 
     @GetMapping
     public String getProduct() {
-        return "HTTP GET Handled";
+        return "HTTP GET Handled with instance:"+ env.getProperty("local.server.port");
     }
 
     @DeleteMapping
