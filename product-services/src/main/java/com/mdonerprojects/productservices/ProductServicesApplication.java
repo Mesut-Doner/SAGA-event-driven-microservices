1572e2c0ea504de82116e4098b9640769d277a91
mesut-dnr <mesutdonerng@gmail.com>
2023-06-19 22:11:40 +0300

    validations added

--- product-services/src/main/java/com/mdonerprojects/productservices/ProductServicesApplication.java
+++ product-services/src/main/java/com/mdonerprojects/productservices/ProductServicesApplication.java
@@ -1,8 +1,12 @@
 package com.mdonerprojects.productservices;
 
+import com.mdonerprojects.productservices.command.interceptor.CreateProductCommandInterceptor;
+import org.axonframework.commandhandling.CommandBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +14,11 @@ public class ProductServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServicesApplication.class, args);
+	}
+
+	@Autowired
+	public void registerCreateProductCommandInterceptor(ApplicationContext context, CommandBus commandBus){
+		commandBus.registerDispatchInterceptor(context.getBean(CreateProductCommandInterceptor.class));
 	}
 
 }
--- product-services/src/main/java/com/mdonerprojects/productservices/command/rest/CreateProductRestModel.java
+++ product-services/src/main/java/com/mdonerprojects/productservices/command/rest/CreateProductRestModel.java
@@ -1,6 +1,9 @@
 package com.mdonerprojects.productservices.command.rest;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +12,12 @@ import java.math.BigDecimal;
 @Data
 @Builder
 public class CreateProductRestModel {
+    @NotBlank(message="Product title is required")
     private String title;
+    @Min(value =1, message = "Price can not be less than 1")
     private BigDecimal price;
+    @Min(value =1, message = "Quantity can not be less than 1")
+    @Max(value =5, message = "Quantity can not be larger than 5")
     private Integer quantity;
 
 }
